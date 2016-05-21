67d3c69fe4ff5cfd0d4f3efec1db2bb74acf61e1
vijaypatha <elderpatha@gmail.com>
2016-05-21 09:31:12 -0600

    a

--- arrayReview.js
+++ arrayReview.js
@@ -3,6 +3,10 @@ var threeItems = [1,2,3];
 //alert the result of your function
 
   //code here
+  var last = function() {
+    return threeItems[threeItems.length - 1];
+  }
+  last();
 
 
 
@@ -10,23 +14,32 @@ var threeItems = [1,2,3];
 
 
 
-//Loop through evenArray removing all values that aren't even 
+//Loop through evenArray removing all values that aren't even
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
   //code here
-
-
+for(var i = 0; i < evenArray.length;i++) {
+  if(evenArray[i] % 2 !== 0) {
+    evenArray.splice(i,1);
+    i--;
+  }
+}
+console.log(evenArray);
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+//below you're given a function that will return a random number between 0 and 30 and an array full or
+//numbers 'randomArray'. Your job is to write a function that will get a random number,
+//then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
   //code here
+
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -58,7 +71,7 @@ var sentence = "Dev Mountain is the best"
 
 
 
-//write a function called capitalize that takes in the myPoem variable and capitalizes every word 
+//write a function called capitalize that takes in the myPoem variable and capitalizes every word
 var myPoem = 'What is a jQuery but a misunderstood object?'
 //What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
 
